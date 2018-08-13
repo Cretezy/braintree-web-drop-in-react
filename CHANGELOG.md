0e3f5b0287c9d26f570214296c05984c7d6fd041
Charles Crete <charles@cretezy.com>
2018-08-13 09:27:11 -0400

    Fix Babel runtime

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## v1.0.5 (2018-08-13)
+
+- Upgrade dependencies
--- src/index.test.js
+++ src/index.test.js
@@ -5,7 +5,7 @@ import DropIn from "./index";
 describe("DropIn", () => {
 	it("Calls dropin.create on instantiation", () => {
 		const braintreeWebDropIn = require("braintree-web-drop-in");
-		braintreeWebDropIn.create = jest.genMockFunction();
+		braintreeWebDropIn.create = jest.fn();
 
 		const component = renderer.create(
 			<DropIn options={{ authorization: "bogus" }} />
@@ -17,7 +17,7 @@ describe("DropIn", () => {
 
 	it("Calls teardown on destruction", done => {
 		const braintreeWebDropIn = require("braintree-web-drop-in");
-		const teardownMock = jest.genMockFunction();
+		const teardownMock = jest.fn();
 		braintreeWebDropIn.create = () =>
 			new Promise(resolve => {
 				resolve({ teardown: teardownMock });
