c8e40ff36100f4f315fb69af8263bfda4741aecf
Francis Paulin <paulinfrancis@gmail.com>
2019-08-30 20:05:35 +0200

    PR feedback fixes.

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
-declare module 'braintree-web-drop-in-react' {
-  import * as React from 'react';
-  import { Options as Opts, Dropin } from 'braintree-web-drop-in';
+import * as React from 'react';
+import { Options as Opts, Dropin } from 'braintree-web-drop-in';
 
-  export type Options = Omit<Opts, 'container'>;
-  export type DropinInstance = Dropin;
+export type Options = Omit<Opts, 'container'>;
+export type DropinInstance = Dropin;
 
-  interface Props {
-    options: Options;
-    onInstance: (instance: Dropin) => void;
-  }
-  
-  export default class DropIn extends React.Component<Props> { }
+interface DropInProps {
+  options: Options;
+  onInstance: (instance: Dropin) => void;
 }
+
+export default class DropIn extends React.Component<DropInProps> { }
